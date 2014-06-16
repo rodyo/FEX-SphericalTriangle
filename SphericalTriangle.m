0fff95414054a559a0e194f0b459a222d08cafd9
Rody Oldenhuis <oldenhuis@gmail.com>
2014-06-16 10:03:50 +0200

    Updated licence info.

--- SphericalTriangle.m
+++ SphericalTriangle.m
@@ -11,7 +11,7 @@ classdef SphericalTriangle < handle % < SphericalPolygon?
 % E-mail     : oldenhuis@gmail.com    (personal)
 %              oldenhuis@luxspace.lu  (professional)
 % Affiliation: LuxSpace sàrl
-% Licence    : GPL + anything implied by placing it on the FEX
+% Licence    : BSD
 
 
 % If you find this work useful, please consider a donation:
